1b8c0bfedb1a428216d5ecf0a21fd500a657ef6a
Daniel Christopher Treidene <daniel.c.treidene@gmail.com>
2014-02-27 21:38:03 +0100

    Android project initialized

---
+++ IndexerIOS/paintcode/GalleryIcon.cs
@@ -0,0 +1,15 @@
+using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using MonoTouch.CoreGraphics;
+
+namespace GarageIndex
+{
+	public class GalleryIcon : UIView
+	{
+		public GalleryIcon(){
+		}
+
+	}
+}
+
---
+++ IndexerIOS/paintcode/WandIcon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace GarageIndex
+{
+	public class EmptyClass : UIImageView
+	{
+		public EmptyClass ()
+		{
+		}
+	}
+}
+
